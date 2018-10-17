3955b1e794a76151d4f94adca3e8ec3fc328e487
Brian O'Carroll <bocarroll36@gmail.com>
2018-10-16 23:54:11 -0700

    modified routes

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var express = require('express');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const morgan = require("morgan");
+// const path=require('path');
 
-var app = express();
+mongoose.Promise = global.Promise;
 
-const {router: usersRouter} = require('./routes/users')
+const {router: usersRouter} = require('./routes/users');
+const {router: authRouter, localStrategy, jwtStrategy} = require('./routes/auth')
+const { router: stockPullRouter } = require('./routes/stockpull/newrouter');
+const { router: portfolioRouter} = require('./routes/portfolio')
 
+const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require("./config");
+
+const app = express();
+
+app.use(morgan('common'));
 //CORS
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -15,15 +30,77 @@ app.use(function (req, res, next) {
     next();
   });
   
+  //Passport JWT
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/users', usersRouter);
+app.use('/auth/', authRouter);
+app.use('/stockpull/', stockPullRouter);
+app.use('/portfolio/', portfolioRouter);
+
+const jwtAuth = passport.authenticate("jwt", { session: false }
+);
+// app.use(express.static(path.join(__dirname, '/public')));
+
+//Test Protected endpoint
+app.get("/protected", jwtAuth, (req, res) => {
+  return res.json({
+    data: "Snoopy"
+  });
+});
+
+app.use(express.json());
 app.use(express.static("public"));
 
-app.use('/users/', usersRouter);
- 
-  if (require.main === module) {
-    app.listen(process.env.PORT || 8080, function() {
-      console.info(`App listening on ${this.address().port}`);
+let server; 
+function runServer(){
+  const PORT = process.env.PORT || 8080;
+  return new Promise((resolve,reject)=>{
+    mongoose.connect(DATABASE_URL,error =>{
+      if(error){
+        return reject(error);
+      }
+    server = app.listen(PORT, function(){
+      console.log(`Port is listening on ${process.env.PORT || 8080}`)
+      resolve(server);
+    })
+    .on('error',function(error){
+      mongoose.disconnect();
+      console.log('we got an error on our hands, roger')
+      reject(error);
     });
-  }
-  
-  
-  module.exports = app;
+  });
+})
+}
+
+
+//Close Server
+function closeServer(){
+  return mongoose.disconnect().then(()=>{
+
+  return new Promise((resolve,reject)=>{
+    console.log('server is closing')
+    server.close(err =>{
+      if(error => {
+        return reject(error);
+      })
+      resolve();
+    })
+    });
+  });
+};
+
+
+
+
+if (require.main === module) {
+  runServer(DATABASE_URL)
+    .catch(err => {
+      console.error('Unable to start the server.')
+      console.error(err)
+    })
+};
+
+
+module.exports = {runServer,closeServer,app};
