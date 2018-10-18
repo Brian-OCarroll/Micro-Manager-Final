f152c9e2dc965d5e3fa550e6899b6010fa3f327a
Brian O'Carroll <bocarroll36@gmail.com>
2018-10-18 14:00:13 -0700

    modified signup page to use await and async

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
+"use strict";
 
+// Listener for Login
 $(document).ready(function () {
     $('.tab a').on('click', function (e) {
       e.preventDefault();
@@ -9,115 +11,82 @@ $(document).ready(function () {
       $(href).fadeIn(500);
     });
   });
-  
-  //listen for signup
-  
-  $('#signup').on('click', function(e) {
-    // e.stopPropogation();
-    e.preventDefault();
-    //add class
-    let username = $('[name="username"]').val();
-    let password = $('[name="pass"]').val();
-    let checkPass = $('[name="checkpass"]').val();
-    if (password !== checkPass) {
-      alert('Passwords do not match!');
-      return;
-    }
-    //call server to create account
-    const options= {
-      url:'/users',
-      type: 'POST',
-      contentType: "application/json; charset=utf-8",
-      dataType:'json',
-      data:JSON.stringify({
+
+
+$("#Login").click(e => {
+  e.preventDefault();
+  let username = $('[name="loguser"]').val();
+  let password = $('[name="logpass"]').val();
+  if (username === "") {
+    alert(`Please enter valid username`);
+    return;
+  }
+  if (password === "") {
+    alert(`Please enter a valid password`);
+    return;
+  }
+  (async () => {
+    let token = await $.ajax({
+      url: "/auth/login",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
         username,
         password
       })
-    };
-  
-    $.ajax(options)
-    .then(function(data) {
-      console.log('User registration succeeded');
-      return data;
-    })
-    .then(function(data){
-      const options2={
-        url:'/auth/login',
-        type: 'POST',
-        contentType: "application/json; charset=utf-8",
-        dataType: 'json',
-        data: JSON.stringify({
-          username,
-          password
-        })
-      };
-  
-      $.ajax(options2)
-      .then(function(loginData){
-        //what the hell this do????????
-        localStorage.setItem("token", token.authToken);
-        localStorage.setItem("user_id", token.user);
-        window.location.replace("./stocksearch.html");
-      })
-      .catch(err => {
-        if (err.responseJSON.message) {
-          alert(err.responseJSON.message);
-          return;
-        }
-        else{
-          alert('Internal Server Error');
-          return;
-        }
-      })
-    })
-    .catch(err => {
-      console.log(err);
-      if(err.responseJSON.message){
-        alert(err.responseJSON.message);
-        return;
-      }
-      else{
-        alert('Internal Server Error');
-        return;
+    });
+    console.log(token.authToken);
+    localStorage.setItem("token", token.authToken);
+    localStorage.setItem("user_id", token.user);
+    window.location.replace("/index.html");
+  })();
+});
+
+// Listener of Signup
+
+$("#Signup").click(e => {
+  e.preventDefault();
+  let username = $('[name="username"]').val();
+  let password = $('[name="pass"]').val();
+  let cpassword = $('[name="checkpass"]').val();
+  if (password !== cpassword) {
+    alert(`Please match sure your passwords match!`);
+    return;
+  }
+  if (password === "" || cpassword === "") {
+    alert(`Please enter a password and confirm it`);
+    return;
+  }
+  if (username === "") {
+    alert(`Please enter valid email`);
+    return;
+  }
+
+  //Create New User Request
+  (async () => {
+    await $.ajax({
+      url: "/users",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
+        username,
+        password
+      }),
+      error: function(err) {
+        console.log(`Error!`, err);
       }
     });
-  });
-  
-  
-  //listen for login
-  $('#login').on('click', function(e) {
-    // e.stopPropogation();
-    e.preventDefault();
-    let username = $('[name="loguser"]').val();
-    let password = $('[name="logpass"]').val();
-    //call server login
-    const options = {
-      url: '/auth/login',
-      type: 'POST',
-      contentType: "application/json; charset=utf-8",
-      dataType: 'json',
+    let token = await $.ajax({
+      url: "/auth/login",
+      method: "POST",
+      contentType: "application/json",
       data: JSON.stringify({
-        "username": username,
-        "password": password
+        username,
+        password
       })
-    };
-  
-    $.ajax(options)
-    .then(function(data) {
-      //console.log('user login succeeded');
-  
-      console.log(token.authToken);
-      localStorage.setItem("token", token.authToken);
-      localStorage.setItem("user_id", token.user);
-      window.location.replace("./stocksearch.html");
-    })
-    .catch(err=>{
-          console.log(err);
-          if(err.status===401){
-        alert("Username or Password is not correct");
-        return;
-          }
-    })
-  });
-  
-  
+    });
+    localStorage.setItem("token", token.authToken);
+    localStorage.setItem("user_id", token.user);
+    window.location.replace("/index.html");
+  })();
+});
